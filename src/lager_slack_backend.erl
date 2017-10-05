c1f8ef79c11aee999da881996b210313e3d455d5
Andrew S <furmanoff@gmail.com>
2017-10-05 19:29:20 +0300

    Using proper lager colors. Added optional signature.

--- src/lager_slack_backend.erl
+++ src/lager_slack_backend.erl
@@ -12,7 +12,8 @@
 %%
 -record(state, {
     level :: term(),
-    uri :: string()
+    uri :: string(),
+    sign=undefined :: string()
 }).
 
 %% lager
@@ -26,25 +27,16 @@ config_to_id(Config) ->
 
 %% gen_event
 init(Config) ->
-    Level = proplists:get_value(level, Config, critical),
-    try {proplists:get_value(uri, Config), lager_util:config_to_mask(Level)} of
-        {Uri, Levels} when is_list(Uri) -> 
-            {ok, #state{uri=Uri, level=Levels}};
-        {Uri, Levels} when is_binary(Uri) -> 
-            {ok, #state{uri=binary_to_list(Uri), level=Levels}};
-        _ -> 
-            {error, {fatal, bad_uri}}
-    catch
-        _:_ -> 
-            {error, {fatal, bad_log_level}}
-    end.
+    config(Config, #state{
+        level = lager_util:level_to_num(critical)
+    }).
 
 handle_call(get_loglevel, S=#state{level=Level}) ->
     {ok, Level, S};
 handle_call({set_loglevel, Level}, S) ->
     try lager_util:config_to_mask(Level) of
-        Levels -> 
-            {ok, ok, S#state{level=Levels}}
+        Mask -> 
+            {ok, ok, S#state{level=Mask}}
     catch
         _:_ -> 
             {ok, {error, bad_log_level}, S}
@@ -52,17 +44,19 @@ handle_call({set_loglevel, Level}, S) ->
 handle_call(_Request, State) ->
     {ok, ok, State}.
 
-handle_event({log, Message}, S=#state{level=Level, uri=Uri}) ->
+handle_event({log, Message}, S=#state{level=Level, uri=Uri, sign=Sign}) ->
     case lager_util:is_loggable(Message, Level, ?MODULE) of
         true ->
             Msg = list_to_binary(lager_msg:message(Message)),
+            Severity = lager_msg:severity(Message),
             {Mega, Sec, _Micro} = lager_msg:timestamp(Message),
             Json = #{
                 attachments => [#{
                     fallback => Msg,
                     text => Msg,
-                    title => lager_msg:severity(Message),
-                    color => color(lager_msg:severity(Message)),
+                    title => Severity,
+                    color => color(Severity),
+                    footer => case Sign of undefined -> null; _ -> Sign end,
                     ts => Mega * 1000000 + Sec
                 }]
             },
@@ -86,11 +80,32 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 %%
-color(debug) -> <<"#DFF2BF">>;
-color(info) -> <<"#BDE5F8">>;
-color(notice) -> <<"#00529B">>;
-color(warning) -> <<"#FEEFB3">>;
-color(error) -> <<"#FFBABA">>;
-color(critical) -> <<"#D8000C">>;
-color(alert) -> <<"#D8000C">>;
-color(emergency) -> <<"#D8000C">>.
+config([{uri, Uri} | T], S) when is_list(Uri) ->
+    config(T, S#state{uri = Uri});
+config([{uri, Uri} | T], S) when is_binary(Uri) ->
+    config(T, S#state{uri = binary_to_list(Uri)});
+config([{uri, _} | _], _) ->
+    {error, bad_uri};
+config([{level, Level} | T], S) ->
+    try lager_util:config_to_mask(Level) of
+        Mask -> 
+            config(T, S#state{level=Mask})
+    catch
+        _:_ -> 
+            {error, bad_log_level}
+    end;
+config([{sign, Sign} | T], S) when is_list(Sign) ->
+    config(T, S#state{sign = Sign});
+config([], State) ->
+    State;
+config(_, _) ->
+    {error, bad_config}.
+
+color(debug) -> null;
+color(info) -> <<"good">>;
+color(notice) -> <<"good">>;
+color(warning) -> <<"warning">>;
+color(error) -> <<"danger">>;
+color(critical) -> <<"danger">>;
+color(alert) -> <<"danger">>;
+color(emergency) -> <<"danger">>.
